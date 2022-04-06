3f6f918bba6f37b906887ff3cff4438bfb3b62d6
Asus <Asus@DESKTOP-F0SLSJ8>
2022-04-06 19:46:40 +0300

    Add project files.

---
+++ HW11Library/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW12Library
+{
+    public class Client
+    {        
+        public void SubscribeNews(NewsProvider newsProvider)
+        {
+            newsProvider.News += new NewsProvider.NewsWriter(Print); 
+        }
+        public void SubscribeWeather(NewsProvider newsProvider)
+        {
+            newsProvider.Weather += new NewsProvider.NewsWriter(Print);
+        }
+        public void SubscribeSports(NewsProvider newsProvider)
+        {
+            newsProvider.Sports += new NewsProvider.NewsWriter(Print);
+        }
+        public void SubscribeAccidents(NewsProvider newsProvider)
+        {
+            newsProvider.Accidents += new NewsProvider.NewsWriter(Print);
+        }
+        public void SubscribeHumor(NewsProvider newsProvider)
+        {
+            newsProvider.Humor += new NewsProvider.NewsWriter(Print);
+        }
+        private void Print(string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
---
+++ HW11Library/NewsProvider.cs
@@ -0,0 +1,46 @@
+﻿namespace HW12Library
+{
+    public class NewsProvider
+    {
+        
+        public string news { get; set; }
+        public string weather { get; set; }
+        public string sports { get; set; }
+        public string accidents { get; set; }
+        public string humor { get; set; }
+        
+        public delegate void NewsWriter(string value);
+        public event NewsWriter News;
+        public event NewsWriter Weather;
+        public event NewsWriter Sports;
+        public event NewsWriter Accidents;
+        public event NewsWriter Humor;
+        public void AddNews(string value)
+        {
+            news = value;
+            News (news);
+        }
+        public void AddWeather (string value)
+        {
+            weather = value;
+            Weather (weather);
+        }
+        public void AddSports (string value)
+        {
+            sports = value;
+            Sports (sports);
+        }
+        public void AddAccidents (string value)
+        {
+            accidents = value;
+            Accidents (accidents);
+        }
+        public void AddHumor (string value)
+        {
+            humor = value;
+            Humor (humor);
+        }
+        
+
+    }
+}
